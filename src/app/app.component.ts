5ad653ce250cacdde5d4a9eb309f460e91d3555f
fernandolucas <fernandolucass.2012@gmail.com>
2019-06-03 21:33:30 -0300

    create menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,18 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './core/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  pages: { url: string, direction: string, icon: string, text: string }[];
+  user: firebase.User;
+
   constructor(
+    private _authService: AuthService,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
@@ -18,6 +23,13 @@ export class AppComponent {
   }
 
   initializeApp() {
+    this.pages = [
+      { url: '/tasks', direction: 'back', icon: 'checkmark', text: 'Tarefas' },
+      { url: '/tasks/create', direction: 'forward', icon: 'add', text: 'Nova Tarefa' }
+    ];
+
+    this._authService.authState$.subscribe(user => (this.user = user))
+
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { MenuToogleComponent } from './components/menu-toogle/menu-toogle.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MenuToogleComponent],
+  imports: [IonicModule],
   exports: [
+    IonicModule,
     CommonModule,
     ReactiveFormsModule,
-    IonicModule,
+    MenuToogleComponent
   ]
 })
 export class SharedModule { }
